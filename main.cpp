6131561f2dc1d46fded9830cb1a0b2e09872de6d
Adxamjon05 <161422394+Adxamjon05@users.noreply.github.com>
2024-04-02 12:14:35 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,21 @@ int product(int num11, int num12){
     return num11*num12;
 }
 
+float acc(int v1, int v2, int t){
+    return (v2-v1)/t;
+}
+
 int main() {
     cout<<"Problem 1\n";
     int num11, num12;
     cin>>num11>>num12;
     cout<<product(num11, num12)<<endl;
+
+    cout<<"Problem 2\n";
+    int v1, v2, t;
+    cin>>v1>>v2>>t;
+    cout<<acc(v1,v2,t)<<endl;
+
+    
     return 0;
 }
